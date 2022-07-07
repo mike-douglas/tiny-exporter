9530d21ed06edd24ea9709c271d6beb7361fc5c7
Mike Douglas <1015725+mike-douglas@users.noreply.github.com>
2022-07-07 14:44:21 -0400

    Incremental work

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -1,29 +1,39 @@
 
 from unicodedata import numeric
 
-def labels_to_string(labels):
+def labels_to_string(labels: list) -> str:
     return ','.join([
         '{}="{}"'.format(k, v) for k, v in labels.items()
     ])
+
+class MetricType(object):
+    Histogram = 'histogram'
+    Gauge = 'gauge'
+    Counter = 'counter'
+    Unknown = 'unknown'
 
 class Metric(object):
     """A base class for a Metric storing ONE value"""
     def __init__(self, name: str, labels: list = [], help: str = '') -> None:
         self.name = name
         self.labels = labels
+        self.type = MetricType.Unknown
 
     def record_value(self, value: int, labels: list = []) -> None:
         self.value = value
 
+    def export_header(self) -> str:
+        return ('# HELP {name} {help}',
+                '# TYPE {name} {type}').format(
+                    **self.__dict__
+                )
+
     def export(self) -> str:
-        return ('# HELP {name} {help}'
-                '# TYPE {name} {type}'
-                '{name}{{labels}} {value}').format(
+        return '{name}{{labels}} {value}'.format(
                     name=self.name,
-                    labels=labels_to_string(self.labels),
-                    help=self.help,
-                    value=self.value
+                    value=self.value,
+                    labels=labels_to_string(self.labels)
                     )
 
     def __str__(self) -> str:
-        return '{0}{1}'.format(self.name, self.labels)
+        return '{0}{1}'.format(self.name, labels_to_string(self.labels))
--- metrics/gauge.py
+++ metrics/gauge.py
@@ -1,4 +1,13 @@
-from metrics import Metric
+from collections import defaultdict
+
+from metrics import Metric, MetricType, labels_to_string
 
 class GaugeMetric(Metric):
-    pass
+    def __init__(self, name: str, labels: list = [], help: str = '') -> None:
+        super.__init__(self, name, labels, help)
+
+        self.type = MetricType.Gauge
+        self.values = defaultdict(lambda: 0)
+    
+    def record_value(self, value: int, labels: list = []) -> None:
+        self.values[labels_to_string(labels + self.labels)] = value
